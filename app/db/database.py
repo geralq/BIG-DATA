d6ce83495bb200f8d25ffa60dcdab48d1ca8425b
Jakub Nagy <nagyjak1@fit.cvut.cz>
2023-10-05 00:07:14 +0100

    configuration of db connection

--- app/db/database.py
+++ app/db/database.py
@@ -1,2 +1,20 @@
 from sqlalchemy import create_engine
-engine = create_engine("sqlite://", echo=True)
+
+import config
+
+from sqlalchemy import URL
+
+from app.db.model import Base
+
+config = config.Config("app/db/database.ini")
+
+url_object = URL.create(
+    "postgresql",
+    username=config.get("user"),
+    password=config.get("password"),
+    host=config.get("host"),
+    port=config.get("port"),
+    database=config.get("dbname")
+)
+
+engine = create_engine(url_object)
--- app/main.py
+++ app/main.py
@@ -1,14 +1,5 @@
-from db.create_table import create_tables
-from db.drop_table import drop_tables
-
-import indexer.document_processor as document_processor
+from app.db import database
+from app.db.model import Base
 
 if __name__ == "__main__":
-    try:
-        drop_tables()
-    except Exception as error:
-        print(error)
-
-    create_tables()
-
-    document_processor.index_document(500)
+    Base.metadata.create_all(database.engine)
